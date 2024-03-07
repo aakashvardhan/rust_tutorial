e0276ac0720157d334d09c6a08ad50a6f2b20110
aakashv100 <vardhan.aakash1@gmail.com>
2024-03-07 19:14:30 +0400

    Add example_pub and foo crates with their respective source files

---
+++ week_2/example_pub/src/main.rs
@@ -0,0 +1,9 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+fn main() {
+    // Example usage of the add function
+    let sum = add(2, 3);
+    println!("The sum is: {}", sum);
+}
